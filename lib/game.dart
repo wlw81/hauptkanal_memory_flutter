115ef6c51a03a552026ecb05c9a66063be775c4d
wlw81 <thomas.pasligh@gmail.com>
2020-04-23 15:49:35 +0200

    Initial commit

---
+++ lib/game.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Game extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Image.asset('assets/hauptkanalLinks/image000.jpg')),
+    );
+  }
+}
