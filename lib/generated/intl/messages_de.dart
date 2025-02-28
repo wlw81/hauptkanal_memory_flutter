cdb3a2004f15a605abe867e030dac0d1072ad5eb
wlw81 <thomas.pasligh@gmail.com>
2025-02-28 18:30:18 +0100

    Update app icons
    - Update iOS app icon configuration.
    - Update Android app icon reference.

---
+++ lib/generated/intl/messages_de.dart
@@ -0,0 +1,48 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a de locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'de';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "about": MessageLookupByLibrary.simpleMessage("Über Hauptkanal Shuffle"),
+    "app_name": MessageLookupByLibrary.simpleMessage("Hauptkanal Shuffle"),
+    "introduction": MessageLookupByLibrary.simpleMessage(
+      "Schaffst du es bis zum Bahnhof?\nTippe immer auf das nächste Haus und knacke den High-Score!",
+    ),
+    "lastScore": MessageLookupByLibrary.simpleMessage("Letzte Punktzahl"),
+    "left2018": MessageLookupByLibrary.simpleMessage("Hauptkanal Links 2018"),
+    "left2022": MessageLookupByLibrary.simpleMessage(
+      "Hauptkanal Links 2022 (bei Nacht)",
+    ),
+    "legal": MessageLookupByLibrary.simpleMessage(
+      "Coding & Design: Thomas Pasligh (2022 photos: Philip Schöppe, QA: klokodax, music by Mrthenoronha)\nSämtliche dargestellten Bilder, Marken, Urheber- & Eigentumsrechte verbleiben bei ihren jeweiligen Eigentümern.",
+    ),
+    "right2018": MessageLookupByLibrary.simpleMessage("Hauptkanal Rechts 2018"),
+    "right2022": MessageLookupByLibrary.simpleMessage(
+      "Hauptkanal Rechts 2022  (bei Nacht)",
+    ),
+    "score": MessageLookupByLibrary.simpleMessage("Punkte"),
+    "scoreboard": MessageLookupByLibrary.simpleMessage("Bestenliste"),
+    "selectStreet": MessageLookupByLibrary.simpleMessage(
+      "Wählen Sie eine Straße",
+    ),
+  };
+}
