27b1d53bfcb419ba0d7c23d07b4ffbe34cafd2a1
wlw81 <thomas.pasligh@gmail.com>
2025-03-03 20:42:55 +0100

    Updates: Migrate to Hauptkanal Shuffle
    -   Renamed the app from "hauptkanalmemory" to "Hauptkanal Shuffle".
    -   Adjusted imports to match the new project name.
    -   Updated dependencies: intl and fake_async.
    -   Incremented version number to 1.1.4+41.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,9 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:HauptkanalShuffle/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hauptkanalmemory/main.dart';
 
 
 void main() {
